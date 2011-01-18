31a96586fff0b0fd76197dde244b113acd33f528
Gregor Weckbecker <gregor@feiner-fug.net>
2011-01-19 00:38:31 +0100

    refactored Sound class and AudioFile class. Created fadeIn and fadeOut functionality

---
+++ PhoneGapLib/Classes/AudioFile.m
@@ -0,0 +1,123 @@
+//
+//  AudioFile.m
+//  PhoneGapLib
+//
+//  Created by normal on 18.01.11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "AudioFile.h"
+
+@implementation AudioFile
+
+@synthesize resourcePath;
+@synthesize resourceURL;
+@synthesize successCallback;
+@synthesize errorCallback;
+@synthesize downloadCompleteCallback;
+@synthesize player;
+@synthesize sound;
+
+- (id) initWithSound:(Sound*)theSound {
+	[super init];
+	self.sound = theSound;
+	return self;
+}
+
+- (void) play {
+	// If audio player is already playing reset it!
+	if(self.player.isPlaying){
+		[self.player stop];
+		self.player.currentTime = 0;
+	}
+	
+	[self.player play];
+	
+	NSLog(@"Playing audio sample '%@'", self.resourcePath);
+}
+
+- (void) stop {
+	[self.player stop];
+	self.player.currentTime = 0;
+	NSLog(@"Stopped playing audio sample '%@'", self.resourcePath);
+}
+
+- (void) pause { 
+	[self.player pause];
+	NSLog(@"Paused playing audio sample '%@'", self.resourcePath);
+}
+
+- (void)performFadeOut {
+	if (numberOfSteps > 0) { 
+		self.player.volume -= step; 
+		numberOfSteps -= 1;
+		[self performSelector:@selector(performFadeOut) withObject:nil afterDelay:0.1];	
+	} else {
+		[self stop];
+		[sound writeJavascript:[NSString stringWithFormat:@"Media.fadeOutCallbacks['%@']();", self.resourcePath]];
+	}		
+}
+
+- (void)fadeOutAndStopAfter:(float)duration {
+	if (self.player.volume == 0 || duration == 0) {
+		[self stop];
+	}
+	
+	numberOfSteps = duration * 10.0;
+	step = self.player.volume / numberOfSteps;
+	[self performSelector:@selector(performFadeOut) withObject:nil afterDelay:0.1];	
+}
+
+- (void)performFadeIn {
+	if (numberOfSteps > 0) {
+		self.player.volume += step; 
+		numberOfSteps -= 1; 
+		[self performSelector:@selector(performFadeIn) withObject:nil afterDelay:0.1];
+	} else {
+		self.player.volume = 1.0;
+		[sound writeJavascript:[NSString stringWithFormat:@"Media.fadeInCallbacks['%@']();", self.resourcePath]];
+	}
+
+}
+
+- (void)playAndFadeInAfter:(float)duration {
+	[self play];
+	
+	self.player.volume = 0;
+	
+	numberOfSteps = duration * 10; 
+	step = 1.0 / numberOfSteps;
+	[self performSelector:@selector(performFadeIn) withObject:nil afterDelay: 0.1];
+}
+
+- (void)audioPlayerDidFinishPlaying:(AVAudioPlayer*)player successfully:(BOOL)flag 
+{
+	NSLog(@"Finished playing audio sample '%@'", resourcePath);
+	
+	[sound removeFromSoundCache:self];
+	
+	if (flag){
+		if (self.successCallback) {
+			NSString* jsString = [NSString stringWithFormat:@"(%@)(\"%@\");", self.successCallback, resourcePath];
+			[sound writeJavascript:jsString];
+		}
+	} else {
+		if (self.errorCallback) {
+			NSString* jsString = [NSString stringWithFormat:@"(%@)(\"%@\");", self.errorCallback, resourcePath];
+			[sound writeJavascript:jsString];
+		}		
+	}
+}
+
+- (void) dealloc
+{
+	self.player = nil;
+	self.successCallback = nil;
+	self.errorCallback = nil;
+	self.downloadCompleteCallback = nil;
+	self.sound = nil;
+	
+	[super dealloc];
+}
+
+@end
--- PhoneGapLib/javascripts/core/media.js
+++ PhoneGapLib/javascripts/core/media.js
@@ -1,71 +1,67 @@
 
-/**
- * Media/Audio override.
- *
- */
- 
-function Media(src, successCallback, errorCallback, downloadCompleteCallback) {
-	
-	if (!src) {
-		src = "documents://" + String((new Date()).getTime()).replace(/\D/gi,''); // random
-	}
-	this.src = src;
-	var successCB = (successCallback == undefined || successCallback == null) ? null : GetFunctionName(successCallback);
-	var errorCB = (errorCallback == undefined || errorCallback == null) ? null : GetFunctionName(errorCallback);
-	var downloadCB = (downloadCompleteCallback == undefined || downloadCompleteCallback == null) ? null : GetFunctionName(downloadCompleteCallback);
-	
-	this.successCallback = successCallback || null;
-	this.errorCallback = errorCallback || null;	
-	this.downloadCompleteCallback = downloadCompleteCallback || null;
-    
-	if (this.src != null) {
-		PhoneGap.exec("Sound.prepare", this.src, successCB, errorCB, downloadCB);
-	}
-}
- 
-Media.prototype.play = function(options) {
-	if (this.src != null) {
-		PhoneGap.exec("Sound.play", this.src, options);
-	}
-}
-
-Media.prototype.pause = function() {
-	if (this.src != null) {
-		PhoneGap.exec("Sound.pause", this.src);
-	}
-}
-
-Media.prototype.stop = function() {
-	if (this.src != null) {
-		PhoneGap.exec("Sound.stop", this.src);
-	}
-}
-
-Media.prototype.startAudioRecord = function(options) {
-	if (this.src != null) {
-		PhoneGap.exec("Sound.startAudioRecord", this.src, options);
-	}
-}
-
-Media.prototype.stopAudioRecord = function() {
-	if (this.src != null) {
-		PhoneGap.exec("Sound.stopAudioRecord", this.src);
-	}
-}
 
 /**
  * This class contains information about any Media errors.
  * @constructor
  */
-function MediaError() {
-	this.code = null,
-	this.message = "";
-}
+function MediaError(code, message) {
+    this.code = code,
+    this.message = message;
+};
 
-MediaError.MEDIA_ERR_ABORTED 		= 1;
-MediaError.MEDIA_ERR_NETWORK 		= 2;
-MediaError.MEDIA_ERR_DECODE 		= 3;
+MediaError.MEDIA_ERR_ABORTED = 1;
+MediaError.MEDIA_ERR_NETWORK = 2;
+MediaError.MEDIA_ERR_DECODE = 3;
 MediaError.MEDIA_ERR_NONE_SUPPORTED = 4;
 
+/**
+ * Media/Audio override.
+ *
+ */
 
-//if (typeof navigator.audio == "undefined") navigator.audio = new Media(src);
+function Media(src, successCallback, errorCallback, downloadCompleteCallback) {
+    this.src = src;
+    this.successCallback = successCallback;
+    this.errorCallback = errorCallback;
+    this.downloadCompleteCallback = downloadCompleteCallback;
+    
+    if (this.src != null) {
+	PhoneGap.exec("Sound.prepare", this.src, this.successCallback, this.errorCallback, this.downloadCompleteCallback);
+    }
+}
+
+Media.fadeInCallbacks = {};
+Media.fadeOutCallbacks = {};
+
+Media.prototype.play = function(options) {
+    if (this.src != null) {
+	PhoneGap.exec("Sound.play", this.src, options);
+    }
+};
+
+Media.prototype.pause = function() {
+    if (this.src != null) {
+	PhoneGap.exec("Sound.pause", this.src);
+    }
+};
+
+Media.prototype.stop = function() {
+    if (this.src != null) {
+	PhoneGap.exec("Sound.stop", this.src);
+    }
+};
+
+Media.prototype.playAndFadeIn = function(duration, callback) {
+    if (this.src != null) {
+	var that = this;
+	Media.fadeInCallbacks[this.src] = callback || function () {};
+	PhoneGap.exec("Sound.playAndFadeIn", this.src, duration);
+    }
+};
+
+Media.prototype.fadeOutAndStop = function(duration, callback) {
+    if (this.src != null) {
+	Media.fadeOutCallbacks[this.src] = callback || function () {};
+	PhoneGap.exec("Sound.fadeOutAndStop", this.src, duration);
+    }
+};
